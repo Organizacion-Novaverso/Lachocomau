2a368d6c99cede8381a6390ea2bce1a7663fca29
fran-cis-ca <fran.cisca.fp14@gmail.com>
2024-02-20 22:35:35 -0300

    Prueba de rama Francisca a rama QA

--- src/components/Main/Bienvenido.jsx
+++ src/components/Main/Bienvenido.jsx
@@ -4,6 +4,7 @@ function Bienvenido() {
     <div className="flex">
       <div>
         <h3>Bienvenidos</h3>
+        <h2>Test Francisca a QA</h2>
       </div>
       <img src="" alt="" />
     </div>
