1bbc1a52bd05e6131d0cb1ae9696dd85627bada4
flan <71607859+fran-cis-ca@users.noreply.github.com>
2024-07-18 23:23:06 -0400

    Comentarios

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,7 +4,7 @@ import NuestrasCategorias from "./NuestrasCategorias"
 import Contenido from "./Contenido"
 //import NuestrasCategorias2 from "./NuestrasCategorias2"
 import Contacto from "./Contacto"
-import Contenido from "./Contenido"
+//import Contenido from "./Contenido"
 
 function Main() {
   return (
